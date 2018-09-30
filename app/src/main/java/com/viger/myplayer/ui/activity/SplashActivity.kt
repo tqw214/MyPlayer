a9454a845a1c9f4020e3a4b1de443b024a6da1c8
tqw214 <tqw214@163.com>
2018-09-30 15:35:17 +0800

    commit

---
+++ app/src/main/java/com/viger/myplayer/ui/activity/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.viger.myplayer.ui.activity
+
+import com.viger.myplayer.base.BaseActivity
+
+class SplashActivity: BaseActivity() {
+
+    override fun getLayoutId(): Int {
+
+        return 0
+    }
+
+}
