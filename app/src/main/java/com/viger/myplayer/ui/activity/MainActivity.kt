ec46b84ddff34308b36606e527fea4dc710a96d9
tqw214 <tqw214@163.com>
2018-09-30 14:28:19 +0800

    commit

--- app/src/main/java/com/viger/myplayer/ui/activity/MainActivity.kt
+++ app/src/main/java/com/viger/myplayer/ui/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.viger.myplayer
+package com.viger.myplayer.ui.activity
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.viger.myplayer.R
 
 class MainActivity : AppCompatActivity() {
 
