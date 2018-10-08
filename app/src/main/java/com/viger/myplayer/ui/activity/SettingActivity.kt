53634ee4df7779ec16a1e1dd9f36f4c36c5c62bd
tqw214 <tqw214@163.com>
2018-10-08 16:19:31 +0800

    commit

---
+++ app/src/main/java/com/viger/myplayer/ui/activity/SettingActivity.kt
@@ -0,0 +1,12 @@
+package com.viger.myplayer.ui.activity
+
+import com.viger.myplayer.R
+import com.viger.myplayer.base.BaseActivity
+
+class SettingActivity : BaseActivity() {
+
+    override fun getLayoutId(): Int {
+        return R.layout.activity_setting
+    }
+
+}
--- app/src/main/java/com/viger/myplayer/util/ToolBarManager.kt
+++ app/src/main/java/com/viger/myplayer/util/ToolBarManager.kt
@@ -1,7 +1,9 @@
 package com.viger.myplayer.util
 
+import android.content.Intent
 import android.support.v7.widget.Toolbar
 import com.viger.myplayer.R
+import com.viger.myplayer.ui.activity.SettingActivity
 
 interface ToolBarManager {
 
@@ -10,6 +12,21 @@ interface ToolBarManager {
     fun initMainToolBar() {
         toolbar.setTitle("我的影音")
         toolbar.inflateMenu(R.menu.main)
+        toolbar.setOnMenuItemClickListener {
+            toolbar.context.startActivity(Intent(toolbar.context, SettingActivity::class.java))
+            true
+        }
+//        toolbar.setOnMenuItemClickListener(object : Toolbar.OnMenuItemClickListener {
+//            override fun onMenuItemClick(item: MenuItem?): Boolean {
+//                when(item?.itemId) {
+//                    R.id.setting->{
+//                        //Toast.makeText(toolbar.context, "点击了设置", Toast.LENGTH_SHORT).show()
+//                        toolbar.context.startActivity(Intent(toolbar.context, SettingActivity::class.java))
+//                    }
+//                }
+//                return true
+//            }
+//        })
     }
 
 }
