1d906a96b69d47bef219d152553aec9255274206
tqw214 <tqw214@163.com>
2018-10-11 15:22:42 +0800

    commit

---
+++ app/src/main/java/com/viger/myplayer/ui/activity/AboutActivity.kt
@@ -0,0 +1,17 @@
+package com.viger.myplayer.ui.activity
+
+import com.viger.myplayer.R
+import com.viger.myplayer.base.BaseActivity
+
+class AboutActivity : BaseActivity() {
+
+    override fun getLayoutId(): Int {
+        return R.layout.activity_about
+    }
+
+    override fun initData() {
+        super.initData()
+
+    }
+
+}
--- app/src/main/java/com/viger/myplayer/ui/fragment/SettingFragment.kt
+++ app/src/main/java/com/viger/myplayer/ui/fragment/SettingFragment.kt
@@ -1,17 +1,31 @@
 package com.viger.myplayer.ui.fragment
 
 import android.os.Bundle
+import android.preference.Preference
 import android.preference.PreferenceFragment
+import android.preference.PreferenceScreen
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.viger.myplayer.R
+import org.jetbrains.anko.toast
 
 class SettingFragment: PreferenceFragment() {
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         addPreferencesFromResource(R.xml.setting)
         return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onPreferenceTreeClick(preferenceScreen: PreferenceScreen?, preference: Preference?): Boolean {
+        val key = preference?.key
+        if("clear_cache".equals(key)) {
+            toast("点击了清除缓存")
+        }
+        if("about".equals(key)) {
+            toast("点击了关于")
+        }
+        return super.onPreferenceTreeClick(preferenceScreen, preference)
     }
 
 }
