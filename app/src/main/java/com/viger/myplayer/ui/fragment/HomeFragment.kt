50a49329cb90e83bff37d6919f1b96604c45f068
tqw214 <tqw214@163.com>
2018-10-12 14:09:57 +0800

    commit

---
+++ app/src/main/java/com/viger/myplayer/ui/fragment/HomeFragment.kt
@@ -0,0 +1,19 @@
+package com.viger.myplayer.ui.fragment
+
+import android.graphics.Color
+import android.view.Gravity
+import android.view.View
+import android.widget.TextView
+import com.viger.myplayer.base.BaseFragment
+
+class HomeFragment : BaseFragment() {
+
+    override fun initView(): View? {
+        val tv = TextView(context)
+        tv.gravity = Gravity.CENTER
+        tv.setTextColor(Color.RED)
+        tv.text = javaClass.simpleName
+        return tv
+    }
+
+}
