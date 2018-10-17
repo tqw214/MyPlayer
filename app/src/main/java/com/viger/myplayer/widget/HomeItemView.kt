5623d67a28999ccb5c1ae1d86be7158df157b4a9
tqw214 <tqw214@163.com>
2018-10-17 16:22:11 +0800

    commit

--- app/src/main/java/com/viger/myplayer/widget/HomeItemView.kt
+++ app/src/main/java/com/viger/myplayer/widget/HomeItemView.kt
@@ -2,12 +2,18 @@ package com.viger.myplayer.widget
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.View
 import android.widget.RelativeLayout
+import com.viger.myplayer.R
 
 class HomeItemView:RelativeLayout {
 
     constructor(context: Context?) : super(context)
     constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
     constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+
+    init {
+        View.inflate(context, R.layout.item_home, this)
+    }
 
 }
