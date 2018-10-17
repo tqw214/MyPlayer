3e4a848c9e7042b48c16aed5e508fa2b816cd605
tqw214 <tqw214@163.com>
2018-10-17 16:15:13 +0800

    commit

--- app/src/test/java/com/viger/myplayer/Test01.java
+++ app/src/test/java/com/viger/myplayer/Test01.java
@@ -3,6 +3,7 @@ package com.viger.myplayer;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
+//测试类
 public class Test01 {
 
     public void md5(String message) throws NoSuchAlgorithmException {
