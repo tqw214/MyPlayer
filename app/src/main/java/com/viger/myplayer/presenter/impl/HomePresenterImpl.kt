00247d7626a38dba53120b559fad86a335b54ffa
tqw214 <tqw214@163.com>
2018-10-24 14:45:18 +0800

    commit

--- app/src/main/java/com/viger/myplayer/presenter/impl/HomePresenterImpl.kt
+++ app/src/main/java/com/viger/myplayer/presenter/impl/HomePresenterImpl.kt
@@ -1,6 +1,7 @@
 package com.viger.myplayer.presenter.impl
 
 import com.viger.myplayer.presenter.interf.HomePresenter
+import com.viger.myplayer.view.HomeView
 
-class HomePresenterImpl: HomePresenter {
+class HomePresenterImpl(var homeView:HomeView): HomePresenter {
 }
