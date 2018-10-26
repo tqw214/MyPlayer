1bd1d56640e6f2d51d06e72f8437ecb9abe17725
tqw214 <tqw214@163.com>
2018-10-26 15:31:05 +0800

    commit

--- app/src/main/java/com/viger/myplayer/presenter/impl/HomePresenterImpl.kt
+++ app/src/main/java/com/viger/myplayer/presenter/impl/HomePresenterImpl.kt
@@ -1,16 +1,10 @@
 package com.viger.myplayer.presenter.impl
 
-import com.google.gson.Gson
-import com.google.gson.reflect.TypeToken
 import com.itheima.player.model.bean.HomeItemBean
 import com.viger.myplayer.net.HomeRequest
 import com.viger.myplayer.net.ResponseHandler
 import com.viger.myplayer.presenter.interf.HomePresenter
-import com.viger.myplayer.util.ThreadUtil
-import com.viger.myplayer.util.URLProviderUtils
 import com.viger.myplayer.view.HomeView
-import okhttp3.*
-import java.io.IOException
 
 class HomePresenterImpl(var homeView:HomeView): HomePresenter {
 
@@ -55,31 +49,39 @@ class HomePresenterImpl(var homeView:HomeView): HomePresenter {
 
     override fun loadMore(offset: Int) {
         //加载更多数据
-        val path = URLProviderUtils.getHomeUrl(offset,20)
-        val client = OkHttpClient()
-        val request = Request.Builder()
-                .url(path)
-                .get()
-                .build()
-        client.newCall(request).enqueue(object : Callback {
-            override fun onFailure(call: Call, e: IOException) {
-                ThreadUtil.runOnMainThread(object :  Runnable{
-                    override fun run() {
-                        homeView.onError(e?.message)
-                    }
-                })
+        val request = HomeRequest(offset, object : ResponseHandler<List<HomeItemBean>>{
+            override fun onError(msg: String?) {
+                homeView.onError(msg)
             }
-            override fun onResponse(call: Call, response: Response) {
-                val result = response.body()?.string()
-                val gson = Gson()
-                val list = gson.fromJson<List<HomeItemBean>>(result, object : TypeToken<List<HomeItemBean>>() {}.type)
-                //刷新列表
-                ThreadUtil.runOnMainThread(object : Runnable{
-                    override fun run() {
-                        homeView.loadMore(list)
-                    }
-                })
+            override fun onSuccess(result: List<HomeItemBean>) {
+                homeView.loadMore(result)
             }
         })
+//        val path = URLProviderUtils.getHomeUrl(offset,20)
+//        val client = OkHttpClient()
+//        val request = Request.Builder()
+//                .url(path)
+//                .get()
+//                .build()
+//        client.newCall(request).enqueue(object : Callback {
+//            override fun onFailure(call: Call, e: IOException) {
+//                ThreadUtil.runOnMainThread(object :  Runnable{
+//                    override fun run() {
+//                        homeView.onError(e?.message)
+//                    }
+//                })
+//            }
+//            override fun onResponse(call: Call, response: Response) {
+//                val result = response.body()?.string()
+//                val gson = Gson()
+//                val list = gson.fromJson<List<HomeItemBean>>(result, object : TypeToken<List<HomeItemBean>>() {}.type)
+//                //刷新列表
+//                ThreadUtil.runOnMainThread(object : Runnable{
+//                    override fun run() {
+//                        homeView.loadMore(list)
+//                    }
+//                })
+//            }
+//        })
     }
 }
