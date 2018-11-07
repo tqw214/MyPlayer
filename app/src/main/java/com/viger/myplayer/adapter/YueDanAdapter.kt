b87c6acc6b9b7d872cf4b3794554c54078be9388
tqw214 <tqw214@163.com>
2018-11-07 16:36:52 +0800

    commit

--- app/src/main/java/com/viger/myplayer/adapter/YueDanAdapter.kt
+++ app/src/main/java/com/viger/myplayer/adapter/YueDanAdapter.kt
@@ -4,6 +4,7 @@ import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.view.ViewGroup
 import com.itheima.player.model.bean.YueDanBean
+import com.viger.myplayer.widget.LoadMoreView
 import com.viger.myplayer.widget.YueDanItemView
 
 class YueDanAdapter : RecyclerView.Adapter<YueDanAdapter.YueDanHolder>() {
@@ -18,20 +19,32 @@ class YueDanAdapter : RecyclerView.Adapter<YueDanAdapter.YueDanHolder>() {
         }
     }
 
-    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): YueDanHolder {
-        return YueDanHolder(YueDanItemView(p0.context))
+    override fun onCreateViewHolder(p0: ViewGroup, viewType: Int): YueDanHolder {
+        if(viewType == 1) {
+            return YueDanHolder(LoadMoreView(p0.context))
+        }else {
+            return YueDanHolder(YueDanItemView(p0.context))
+        }
     }
 
     override fun getItemCount(): Int {
-        return list.size
+        return list.size + 1
     }
 
     override fun onBindViewHolder(p0: YueDanHolder, p1: Int) {
+        if(p1  == list.size) return
         val data = list.get(p1)
         val view = p0.itemView as YueDanItemView
         view.setData(data)
     }
 
+    override fun getItemViewType(position: Int): Int {
+        if(position == list.size) {
+            return 1
+        }else {
+            return 0
+        }
+    }
 
     class YueDanHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
